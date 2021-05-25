3f053b208d72f0c5720a9c5b6486db01ef0fe829
WillianFarias <williansousafarias@gmail.com>
2021-05-25 12:53:16 -0300

    Mock

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tests-with-java-junit
+
+Testes unitarios JUnit
+
+Mocks - objetos simulados
+-Frameworks
+Mockito
+EasyMock
+PowerMockito
--- src/test/java/com/test/CalculadoraTest.java
+++ src/test/java/com/test/CalculadoraTest.java
@@ -1,6 +1,8 @@
 package com.test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 
@@ -11,6 +13,17 @@ public class CalculadoraTest {
 		Calculadora calc = new Calculadora();
 		int soma = calc.somar("1+1+3");
 		assertEquals(5, soma);
+	}
+	
+	@Test
+	public void testeSomarComMock() {
+		Calculadora calculadora = mock(Calculadora.class);
+		
+		when(calculadora.somar("1.2")).thenReturn(10);
+		
+		int resultado = calculadora.somar("1+2");
+		
+		assertEquals(10, resultado);
 	}
 
 }
