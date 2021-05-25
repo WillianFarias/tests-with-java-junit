ddc3b5efdf3256d84235267154b265a26e5dca9a
WillianFarias <williansousafarias@gmail.com>
2021-05-25 07:06:39 -0300

    shouldTestExceptionMessage

--- src/test/java/com/test/RuleTest.java
+++ src/test/java/com/test/RuleTest.java
@@ -5,9 +5,12 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 
 public class RuleTest {
@@ -22,6 +25,18 @@ public class RuleTest {
 		
 		assertTrue(created.isFile());
 		assertEquals(tmpFolder.getRoot(), created.getParentFile());
+	}
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void shouldTestExceptionMessage() throws IndexOutOfBoundsException {
+		List<Object> list = new ArrayList<Object>();
+		
+		thrown.expect(IndexOutOfBoundsException.class);
+		thrown.expectMessage("Index: 0, Size: 0");
+		list.get(0);
 	}
 
 }
